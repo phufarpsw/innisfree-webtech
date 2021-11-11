7af253527753d2c92b49a74b45bfe487ffca919a
63070014 <63070014@kmitl.ac.th>
2021-11-11 17:11:40 +0700

    update green tea

---
+++ js/ingredient.js
@@ -0,0 +1,152 @@
+// บีจา 
+var ico_blue = document.querySelectorAll(".ico-blue");
+document.querySelector(".bg-blue").addEventListener("mouseover", function(){
+    document.querySelector(".bg-blue").style.fill = "#0aaecc";
+    document.querySelector(".txt-blue").style.fill = "white";
+    for (let i = 0; i < ico_blue.length; i++) {
+        ico_blue[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-blue").addEventListener("mouseout", function(){
+    document.querySelector(".bg-blue").style.fill = "#f9f9f9";
+    document.querySelector(".txt-blue").style.fill = "#757575";
+    for (let i = 0; i < ico_blue.length; i++) {
+        ico_blue[i].style.fill = "#757575";
+    }
+})
+
+
+// ส้มแมนดาริน
+var ico_orange = document.querySelectorAll(".ico-orange");
+document.querySelector(".bg-orange").addEventListener("mouseover", function(){
+    document.querySelector(".bg-orange").style.fill = "#f59700";
+    document.querySelector(".txt-orange").style.fill = "white";
+    for (let i = 0; i < ico_orange.length; i++) {
+        ico_orange[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-orange").addEventListener("mouseout", function(){
+    document.querySelector(".bg-orange").style.fill = "#f9f9f9";
+    document.querySelector(".txt-orange").style.fill = "#757575";
+    for (let i = 0; i < ico_orange.length; i++) {
+        ico_orange[i].style.fill = "#757575";
+    }
+})
+
+
+// ตะกอนหินภูเขาไฟ
+var ico_choco = document.querySelectorAll(".ico-choco");
+document.querySelector(".bg-choco").addEventListener("mouseover", function(){
+    document.querySelector(".bg-choco").style.fill = "#402a00";
+    document.querySelector(".txt-choco").style.fill = "white";
+    for (let i = 0; i < ico_choco.length; i++) {
+        ico_choco[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-choco").addEventListener("mouseout", function(){
+    document.querySelector(".bg-choco").style.fill = "#f9f9f9";
+    document.querySelector(".txt-choco").style.fill = "#757575";
+    for (let i = 0; i < ico_choco.length; i++) {
+        ico_choco[i].style.fill = "#757575";
+    }
+})
+
+
+
+// ถั่วเหลือง
+var ico_brown = document.querySelectorAll(".ico-brown");
+document.querySelector(".bg-brown").addEventListener("mouseover", function(){
+    document.querySelector(".bg-brown").style.fill = "#623d2c";
+    document.querySelector(".txt-brown").style.fill = "white";
+    for (let i = 0; i < ico_brown.length; i++) {
+        ico_brown[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-brown").addEventListener("mouseout", function(){
+    document.querySelector(".bg-brown").style.fill = "#f9f9f9";
+    document.querySelector(".txt-brown").style.fill = "#757575";
+    for (let i = 0; i < ico_brown.length; i++) {
+        ico_brown[i].style.fill = "#757575";
+    }
+})
+
+
+
+// กล้วยไม้
+document.querySelector(".bg-purple").addEventListener("mouseover", function(){
+    document.querySelector(".bg-purple").style.fill = "#300050";
+    document.querySelector(".txt-purple").style.fill = "white";
+    document.querySelector(".ico-purple").style.fill = "white";
+})
+document.querySelector(".bg-purple").addEventListener("mouseout", function(){
+    document.querySelector(".bg-purple").style.fill = "#f9f9f9";
+    document.querySelector(".txt-purple").style.fill = "#757575";
+    document.querySelector(".ico-purple").style.fill = "#757575";
+})
+
+
+
+// ทับทิม
+var ico_red = document.querySelectorAll(".ico-red");
+document.querySelector(".bg-red").addEventListener("mouseover", function(){
+    document.querySelector(".bg-red").style.fill = "#ba0000";
+    document.querySelector(".txt-red").style.fill = "white";
+    for (let i = 0; i < ico_red.length; i++) {
+        ico_red[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-red").addEventListener("mouseout", function(){
+    document.querySelector(".bg-red").style.fill = "#f9f9f9";
+    document.querySelector(".txt-red").style.fill = "#757575";
+    for (let i = 0; i < ico_red.length; i++) {
+        ico_red[i].style.fill = "#757575";
+    }
+})
+
+
+// ป่ากดจาวัล
+document.querySelector(".bg-light-green").addEventListener("mouseover", function(){
+    document.querySelector(".bg-light-green").style.fill = "#83a48a";
+    document.querySelector(".txt-light-green").style.fill = "white";
+    document.querySelector(".ico-light-green").style.fill = "white";
+})
+document.querySelector(".bg-light-green").addEventListener("mouseout", function(){
+    document.querySelector(".bg-light-green").style.fill = "#f9f9f9";
+    document.querySelector(".txt-light-green").style.fill = "#757575";
+    document.querySelector(".ico-light-green").style.fill = "#757575";
+})
+
+
+// สาหร่ายทะเล
+var ico_ocean = document.querySelectorAll(".ico-ocean");
+document.querySelector(".bg-ocean").addEventListener("mouseover", function(){
+    document.querySelector(".bg-ocean").style.fill = "#001b9e";
+    document.querySelector(".txt-ocean").style.fill = "white";
+    for (let i = 0; i < ico_ocean.length; i++) {
+        ico_ocean[i].style.fill = "white";
+    }
+})
+document.querySelector(".bg-ocean").addEventListener("mouseout", function(){
+    document.querySelector(".bg-ocean").style.fill = "#f9f9f9";
+    document.querySelector(".txt-ocean").style.fill = "#757575";
+    for (let i = 0; i < ico_ocean.length; i++) {
+        ico_ocean[i].style.fill = "#757575";
+    }
+})
+
+// ข้าวบาร์เลย์สีเขียว
+var ico_fade_green = document.querySelectorAll(".ico-fade-green");
+document.querySelector(".bg-fade-green").addEventListener("mouseover", function(){
+    document.querySelector(".bg-fade-green").style.fill = "#97b489";
+    document.querySelector(".txt-fade-green").style.fill = "#97b489";
+    for (let i = 0; i < ico_fade_green.length; i++) {
+        ico_fade_green[i].style.fill = "#97b489";
+    }
+})
+document.querySelector(".bg-fade-green").addEventListener("mouseout", function(){
+    document.querySelector(".bg-fade-green").style.fill = "#f9f9f9";
+    document.querySelector(".txt-fade-green").style.fill = "#757575";
+    for (let i = 0; i < ico_fade_green.length; i++) {
+        ico_fade_green[i].style.fill = "#757575";
+    }
+})
